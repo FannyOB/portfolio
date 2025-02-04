2cc4d837d5c4d86c1e450e3c15701e3c977cd121
Fanny <fanny.olengabinene@laposte.net>
2025-02-04 22:01:12 +0100

    add icon for dark theme,create navbar for mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { Outfit, Ovo } from 'next/font/google';
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
@@ -8,9 +10,14 @@ const config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+       lightHover: '#fcf4ff',
+       darkHover: '#2a004a',
+       darkTheme: '#11001F',
       },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      }
     },
   },
   plugins: [],

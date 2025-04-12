1242b701d7d0e867788edc4a9e5151983e077d6c
Fanny <fanny.olengabinene@laposte.net>
2025-04-12 19:54:49 +0200

    adding features for to receive a notification in my mail box after submit the form

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,8 +1,36 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { useState } from 'react'
 
 const Contact = () => {
+
+    const [result, setResult] = useState("");
+
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", process.env.NEXT_PUBLIC_ACCESS_KEY);
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+        } else {
+        console.log("Error", data);
+        setResult(data.message);
+        }
+    };
+
   return (
     <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 
     bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
@@ -13,18 +41,18 @@ const Contact = () => {
         I am always open to new opportunities and collaborations. <br/>If you have a have any questions, feedback or just want to chat, please use the form below.
       </p>
 
-      <form className='max-w-2xl mx-auto'>
+      <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
         <div className='grid grid-cols-2 gap-6 mt-10 mb-8'>
             <input type='text' placeholder="Enter your name" required 
             className='flex-1 p-3 outline-none border-[0.5px] 
-            border-gray-400 rounded-md bg-white'/>
+            border-gray-400 rounded-md bg-white' name='name'/>
             <input type='email' placeholder="Enter your email" required 
             className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 
-            rounded-md bg-white'/>
+            rounded-md bg-white' name='email'/>
         </div>
         <textarea rows='6' placeholder='Enter your message' required 
         className='w-full p-4 outline-none border-[0.5px] border-gray-400 
-        rounded-md bg-white mb-6'></textarea>
+        rounded-md bg-white mb-6' name='message'></textarea>
 
         <button type='submit'
         className='py-3 px-8 w-max flex items-center justify-between gap-2 
@@ -32,7 +60,7 @@ const Contact = () => {
         >Submit now <Image src={assets.right_arrow_white} alt='' className='w-4'/>
         </button>
 
-        <p className='mt-4'>sending....</p>
+        <p className='mt-4'>{result}</p> {/* plus tard, Afficher le message de résultat dans un badge shadcn.ui*/} 
       </form>
     </div>
   )

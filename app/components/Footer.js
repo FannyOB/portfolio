de0c47c6951a2ca5b0854c6cbe5675131ff97d50
Fanny <fanny.olengabinene@laposte.net>
2025-04-15 19:35:39 +0200

    adding div for copyriright, github and linkedin links in the footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -13,6 +13,15 @@ const Footer = () => {
                 fanny.olengabinene@laposte.net
             </div>
         </div>
+
+        <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] 
+        mt-12 py-6'>
+            <p>© 2025 Fanny Olenga Binene. All rigths reserved.</p>
+            <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+                <li><a target='_blank' href='https://github.com/FannyOB'>GitHub</a></li>
+                <li><a target='_blank' href='www.linkedin.com/in/fanny-olenga-binene-'>LinkedIn</a></li>
+            </ul>
+        </div>
     </div>
   )
 }
